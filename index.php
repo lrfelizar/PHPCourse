2289f2cce0e5da1dcf6d8d5e8c2973a70efebfb9
lauprueba <70724873+lauprueba@users.noreply.github.com>
2020-09-03 18:06:25 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if (!$conexion) {
 
 $posts = obtener_post($blog_config['post_por_pagina'], $conexion);
 if (!$posts) {
-    header('Location: error.php');
+    header('Location');
 }
 
 require 'views/index.view.php';
